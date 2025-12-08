72972067c5987310b1fb809b0456e55992476d5a
Faqih Azhar <faqihazh@gmail.com>
2025-12-08 12:18:59 +0700

    first commit

---
+++ lib/utils/http.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const http = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    timeout: 10000,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+});
+
+http.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default http;
---
+++ lib/utils/providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from '@/lib/api/queryClient'
+
+interface ProvidersProps {
+    children: React.ReactNode;
+}
+
+export function QueryProvider({ children }: ProvidersProps) {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            {process.env.NODE_ENV === 'development' && (
+                <ReactQueryDevtools initialIsOpen={false} />
+            )}
+        </QueryClientProvider>
+    );
+}
