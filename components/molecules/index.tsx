269bad7bdba9f901cd098d496bb8a6bccf998730
yusufariiq <yusufariiq3@gmail.com>
2025-12-19 09:48:38 +0700

    feat(tabs): create tabs component

--- components/molecules/index.tsx
+++ components/molecules/index.tsx
@@ -1,3 +1,4 @@
 export { DatePicker, DateRangePicker, type DatePickerProps, type DateRangePickerProps } from "./date-picker/date-picker";
 export { FileUploader, type FileUploaderProps, type UploadedFile } from "./file-uploader/file-uploader";
 export { PaginationController as Pagination, type PaginationControllerProps} from "./pagination/pagination";
+export { Tabs } from "./tabs/tabs";
