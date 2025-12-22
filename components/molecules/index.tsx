6c315f534de2f348186b3fccbc25175305f50fb7
yusufariiq <yusufariiq3@gmail.com>
2025-12-22 10:46:57 +0700

    feat(dropdown): create dropdown component

--- components/molecules/index.tsx
+++ components/molecules/index.tsx
@@ -3,3 +3,20 @@ export { FileUploader, type FileUploaderProps, type UploadedFile } from "./file-
 export { PaginationController as Pagination, type PaginationControllerProps} from "./pagination/pagination";
 export { Tabs, type TabItem, type TabsProps } from "./tabs/tabs";
 export { Combobox, type ComboboxOption, type ComboboxGroup } from "./combobox/combobox"
+export { 
+    DropdownMenu,
+    DropdownMenuPortal,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuSubContent,
+} from "./dropdown/dropdown"
